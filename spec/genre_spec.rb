73186d9547ef45fd1305ff260702ff9d3f1c81d8
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-05 12:19:43 +0300

    Fix linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,23 +1,23 @@
 require_relative '../genre'
 
 describe Genre do
-    let(:genre) { Genre.new(name) }
-    let(:name) { 'Rock' }
-    let (:item) { double('Item') }
+  let(:genre) { Genre.new(name) }
+  let(:name) { 'Rock' }
+  let(:object) { double('Item') }
 
-    context 'Initialize' do
-        it 'sets name' do
-            expect(genre.name).to eq(name)
-        end
-
-        it 'sets empty items array' do
-            expect(genre.items).to be_empty
-        end
-
-        it 'adds item' do
-            allow(item).to receive(:genre=)
-            genre.add_item(item)
-            expect(genre.items).to include(item)
-        end
+  context 'Initialize' do
+    it 'sets name' do
+      expect(genre.name).to eq(name)
     end
+
+    it 'sets empty items array' do
+      expect(genre.items).to be_empty
+    end
+
+    it 'adds item' do
+      allow(object).to receive(:genre=)
+      genre.add_item(object)
+      expect(genre.items).to include(object)
+    end
+  end
 end
