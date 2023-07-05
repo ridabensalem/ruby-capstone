89cc32a6bb991e0e1d6e70702010f960cf25b936
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-07-05 11:56:25 +0300

    Add tests for genre method

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,1 +1,23 @@
 require_relative '../genre'
+
+describe Genre do
+    let(:genre) { Genre.new(name) }
+    let(:name) { 'Rock' }
+    let (:item) { double('Item') }
+
+    context 'Initialize' do
+        it 'sets name' do
+            expect(genre.name).to eq(name)
+        end
+
+        it 'sets empty items array' do
+            expect(genre.items).to be_empty
+        end
+
+        it 'adds item' do
+            allow(item).to receive(:genre=)
+            genre.add_item(item)
+            expect(genre.items).to include(item)
+        end
+    end
+end
